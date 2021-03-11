76b343e3d33bf3f3c3929be8eaa8f43f4a6c58e7
Tomáš Tušer <tomastuser@seznam.cz>
2021-03-11 22:53:00 +0100

    kavyl obecne, overflow maintext a aktualita

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -88,6 +88,10 @@ function Nav() {
       open: false,
       subNavLinks: [
         {
+          name: 'Obecně',
+          path: '/kavyl',
+        },
+        {
           name: 'Kroužky',
           path: '/kavyl/krouzky',
         },
@@ -198,26 +202,49 @@ function Nav() {
                   </NavLink>
                 )}
                 <ul className='subNav'>
-                  {link.subNavLinks.map((subLink) => (
-                    <li
-                      key={subLink.name}
-                      className={
-                        window.innerWidth < 768
-                          ? subNavLinksOpen[Number(link.id) - 1]
-                            ? 'subNavLi subNavActive'
+                  {link.subNavLinks.map((subLink) =>
+                    subLink.name === 'Obecně' ? (
+                      window.innerWidth < 769 && (
+                        <li
+                          key={subLink.name}
+                          className={
+                            window.innerWidth < 769
+                              ? subNavLinksOpen[Number(link.id) - 1]
+                                ? 'subNavLi subNavActive'
+                                : 'subNavLi'
+                              : 'subNavLi'
+                          }
+                        >
+                          <NavLink
+                            className='navLink'
+                            activeClassName='navLinkActive'
+                            to={{ pathname: subLink.path }}
+                          >
+                            <p>{subLink.name}</p>
+                          </NavLink>
+                        </li>
+                      )
+                    ) : (
+                      <li
+                        key={subLink.name}
+                        className={
+                          window.innerWidth < 768
+                            ? subNavLinksOpen[Number(link.id) - 1]
+                              ? 'subNavLi subNavActive'
+                              : 'subNavLi'
                             : 'subNavLi'
-                          : 'subNavLi'
-                      }
-                    >
-                      <NavLink
-                        className='navLink'
-                        activeClassName='navLinkActive'
-                        to={{ pathname: subLink.path }}
+                        }
                       >
-                        <p>{subLink.name}</p>
-                      </NavLink>
-                    </li>
-                  ))}
+                        <NavLink
+                          className='navLink'
+                          activeClassName='navLinkActive'
+                          to={{ pathname: subLink.path }}
+                        >
+                          <p>{subLink.name}</p>
+                        </NavLink>
+                      </li>
+                    )
+                  )}
                 </ul>
               </li>
             ))}
