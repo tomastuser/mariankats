142ad0bf96bdaa6699ae58500a5fd10ea7b3d4e3
tomastuser <tomastuser@seznam.cz>
2023-09-20 16:53:24 +0200

    nove projekty

--- src/pages/ONas/Projekty.tsx
+++ src/pages/ONas/Projekty.tsx
@@ -10,9 +10,11 @@ const Projekty = () => {
       <div className='mainTextCont'>
         <div className='mainText'>
           <Zaklad nazev='Projekty' />
-          <ContentImport nazev='Projekty 0' />
+          <ContentImport nazev='Projekty 1.1' />
+          <FotoCont name='OP JAK Mari 2023.jpg' alt='JAK Mari' />
+          <ContentImport nazev='Projekty 1.2' />
           <FotoCont name='EU.jpg' alt='EU' />
-          <ContentImport nazev='Projekty 1' />
+          <ContentImport nazev='Projekty 1.3' />
           <FotoCont name='projekty.jpg' alt='Projekty 1' />
           <ContentImport nazev='Projekty 2' />
           <FotoCont name='projekty2.jpg' alt='Projekty 2' />
